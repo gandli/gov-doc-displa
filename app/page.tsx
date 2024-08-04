46523db96621ba8029b02f6ba128e5fd5911e075
gandli <gandli@qq.com>
2024-08-05 06:50:08 +0800

    Updated homepage styling and article display for improved readability. Added padding to the page container, enhanced header styles for better hierarchy, and optimized paragraph styling for a cleaner presentation. This refactoring elevates the visual appeal and user experience on the homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -18,13 +18,13 @@ const HomePage = async () => {
 
   // 渲染页面
   return (
-    <div>
+    <div className='p-24'>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">文章列表</h1>
       <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
         {articles.map((article) => (
           <li key={article.id}>
-            <h2>{article.title}</h2>
-            <p>{article.content}</p>
+            <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">{article.title}</h2>
+            <p className="leading-7 [&:not(:first-child)]:mt-6">{article.content}</p>
           </li>
         ))}
       </ul>
