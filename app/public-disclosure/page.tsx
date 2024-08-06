37054f3e5bfeec0495c913eff8fc0f531d4f764e
gandli <gandli@qq.com>
2024-08-06 15:43:17 +0800

    Enhanced PDF rendering for better accessibility and user experience. Adjusted document display properties to ensure consistent width and improved margins for readability. Activated text and annotation layers for enhanced interactivity and accessibility. Updated dependencies and fine-tuned environment settings for compatibility with newer Node.js versions. Streamlined code structure for clarity and maintainability.

--- app/public-disclosure/page.tsx
+++ app/public-disclosure/page.tsx
@@ -2,44 +2,36 @@
 
 import { useState } from 'react';
 import { pdfjs, Document, Page } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import type { PDFDocumentProxy } from 'pdfjs-dist';
-
-type PDFFile = string | File | null;
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
     'pdfjs-dist/build/pdf.worker.min.mjs',
     import.meta.url,
 ).toString();
 
-export default function PublicDisclosure() {
+type PDFFile = string | File | null;
+
+export default function Sample() {
+    const [file, setFile] = useState<PDFFile>('./fwzn.pdf');
     const [numPages, setNumPages] = useState<number>();
     const [scale, setScale] = useState(1.5);
-    const file: PDFFile = './fwzn.pdf'
 
     function onDocumentLoadSuccess({ numPages: nextNumPages }: PDFDocumentProxy): void {
         setNumPages(nextNumPages);
     }
 
     return (
-        <div className="flex justify-center px-4 py-8">
-            <div className="w-full max-w-6xl">
-                <Document
-                    className="flex flex-col items-center"
-                    file={file}
-                    onLoadSuccess={onDocumentLoadSuccess}
-                >
-                    {Array.from(new Array(numPages), (el, index) => (
-                        <Page
-                            key={`page_${index + 1}`}
-                            pageNumber={index + 1}
-                            scale={scale}
-                            className="mb-8 shadow-xl rounded overflow-hidden"
-                            renderTextLayer={false}
-                            renderAnnotationLayer={false}
-                        />
-                    ))}
-                </Document>
-            </div>
-        </div>
+        <Document className="flex flex-col items-center" file={file} onLoadSuccess={onDocumentLoadSuccess} >
+            {Array.from(new Array(numPages), (el, index) => (
+                <Page
+                    key={`page_${index + 1}`}
+                    pageNumber={index + 1}
+                    className="w-md mb-8 shadow-xl rounded overflow-hidden"
+                    scale={scale}
+                />
+            ))}
+        </Document>
     );
 }
